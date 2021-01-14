c72a63a3ee65f21c2cbf79ad0a8e310ed98682a7
BabsBynge <mads164i@edu.sde.dk>
2021-01-14 10:13:34 +0100

    første kode

---
+++ byttepenge.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 08:08:31 2021
+
+@author: madsl
+"""
+
+import math
+
+#nedenstående variabler skal have en billedeværdi når færdig
+Ikr = 1
+IIkr = 2
+Vkr = 5
+Xkr = 10
+Lkr = 50
+Ckr = 100
+CCkr = 200
+Dkr = 500
+Mkr = 1000
+
+
+def difference(a, b):
+    c = a - b
+    return c
+
+time = true
+
+def pay_back(a):
+    if a >= Mkr:
+        
+        
+        
+        
+        
+#hvad skal  de betale?
+pay = int(input("at betale: "))
+
+#hvad betaler de med?
+cos_pay = int(input("betalt: "))
+
+print("skal have",(difference(cos_pay, pay)),"tilbage")
+
+
+
+
